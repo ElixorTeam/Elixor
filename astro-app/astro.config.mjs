d9420ef0647c35dc60beb5c16546f8b808eb8535
Ap73MKa <artyom.vlasov.05@mail.ru>
2023-07-19 14:55:40 +0300

    ADD: basic docker file

--- astro-app/astro.config.mjs
+++ astro-app/astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import solid from "@astrojs/solid-js";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false
-  }), solid()]
+  }), solid()],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  })
 });
